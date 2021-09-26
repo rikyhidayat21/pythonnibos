c1b864019abe8fd3462f94385beec634325bbb41
hdytxr <rikyhidayat21@student.upi.edu>
2021-09-26 16:07:43 +0700

    c4

--- classes/c1.py
+++ classes/c1.py
@@ -71,3 +71,21 @@
 # print("area: " + str(r.area()))
 # print("Perimeter:" + str(r.perimeter()))
 
+### c4: implement a print method
+# Implement a function in the Rectangle class __str__ method, such that when you print one of the objects using the print() command, it prints the coordinates as x1, y1, x2, y2.
+
+class Rectangle:
+  def __init__(self, x1, y1, x2, y2): # class constructor
+    if x1 < x2 and y1 > y2:
+      self.x1 = x1
+      self.y1 = y1
+      self.x2 = x2
+      self.y2 = y2
+    else:
+      print("Incorrect coordinates of the rectangle!")
+        
+  def __str__(self):
+    return(str(self.x1) + ', ' + str(self.y1) + ', ' + str(self.x2) + ', '+str(self.y2))
+  
+r = Rectangle (2, 7, 8, 4)
+print (r)
